3ab10b05f58824bfe8ed65b3a9669496827fae33
isgust <rocha.gustavops@gmail.com>
2024-05-13 11:42:26 -0300

    Usabilidade do Botão de favoritos

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ void main() {
   );
 }
 
-
 class RockInRio extends StatelessWidget {
   const RockInRio({Key? key}) : super(key: key);
 
@@ -33,39 +32,60 @@ class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
 }
+
 class _HomePageState extends State<HomePage> {
+  List<Atracao> listaFavoritos = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
           'Atrações',
-          style: TextStyle(color: Color.fromARGB(255, 250, 250, 250)),
+          style: TextStyle(
+            color: Color.fromARGB(255, 250, 250, 250),
+            fontWeight: FontWeight.bold,
+          ),
         ),
         backgroundColor: Color.fromARGB(219, 0, 0, 0),
         actions: [
           Image.asset(
             'assets/images/logo.png',
-            height: 70,
+            height: 50,
           )
         ],
       ),
       body: ListView.builder(
         itemCount: listaAtracoes.length,
         itemBuilder: (context, index) {
+          final isFavorito = listaFavoritos.contains(listaAtracoes[index]);
           return ListTile(
             title: Text(listaAtracoes[index].nome),
-            subtitle: Wrap(spacing: 8, 
-            runSpacing: 4,
-            children: listaAtracoes[index]
-            .tags.map((tag) => Chip(label: Text('#$tag')))
-            .toList(),
+            subtitle: Wrap(
+              spacing: 8,
+              runSpacing: 4,
+              children: listaAtracoes[index]
+                  .tags
+                  .map((tag) => Chip(label: Text('#$tag')))
+                  .toList(),
             ),
-            leading: CircleAvatar(child: Text('${listaAtracoes[index]}'),
+            leading: CircleAvatar(
+              child: Text('${listaAtracoes[index].nome[0]}'),
             ),
             trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.favorite)
+              onPressed: () {
+                setState(() {
+                  if (isFavorito) {
+                    listaFavoritos.remove(listaAtracoes[index]);
+                  } else {
+                    listaFavoritos.add(listaAtracoes[index]);
+                  }
+                });
+              },
+              icon: Icon(
+                isFavorito ? Icons.favorite : Icons.favorite_border,
+                color: isFavorito ? Colors.red : null,
+              ),
             ),
           );
         },
@@ -73,7 +93,6 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
 
 class Atracao {
   final String nome;
@@ -100,4 +119,3 @@ const listaAtracoes = [
   Atracao("Rihanna", 4, ["R&B", "Moda", "Sucesso"]),
   Atracao("Maroon 5", 11, ["Pop", "Hits", "Estádio"]),
 ];
-
